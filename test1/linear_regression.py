f5e5117278bfbaa6fd509c7a37657b43893ef05c
saoduan <1341915601@qq.com>
2018-06-11 14:31:07 +0800

    2

---
+++ test1/linear_regression.py
@@ -0,0 +1,25 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import mat
+
+
+class LinearRegression:
+    def __init__(self):
+        pass
+
+    def sigmoid(self):
+        pass
+
+    def costFunction(self):
+        pass
+
+    def gradient(self):
+        pass
+
+    def plotDataSet(self):
+        pass
+
+    def plotDecisionBoundary(self):
+        pass
